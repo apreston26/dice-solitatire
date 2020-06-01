96048ffc422a652cd94999327cef712e54d5f7c8
Austin Preston <austinpreston314@gmail.com>
2020-06-01 12:04:18 -0600

    Preparing to display dice images.

--- app/src/main/java/edu/cnm/deepdive/dicesolitatire/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/dicesolitatire/MainActivity.java
@@ -21,54 +21,62 @@ public class MainActivity extends AppCompatActivity {
   private static final String SCRATCH_COUNT_ID_FORMAT = "scratch_%d_count";
 
   private int minPairValue = 2;
-  private int maxPairValue;
-  private int minScratchValue = 2;
+  private int maxPairValue = 2 * Roll.NUM_FACES;
   private TextView[] pairLabels;
   private ProgressBar[] pairCounts;
   private TextView[] scratchLabels;
   private ProgressBar[] scratchCounts;
   private Button roller;
-  private TextView rollDisplay;
-  private Random rng;
+  private Random rng = new Random();
 
   @Override
-
-  //Code for pairs
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setupUI();
+  }
+
+  private void setupUI() {
     setContentView(R.layout.activity_main);
-    maxPairValue = 2 * Roll.NUM_FACES;
+    Resources res = getResources();
+    NumberFormat formatter = NumberFormat.getInstance();
+    setupPairControls(res, formatter);
+    setupPlayControls();
+    setupScratchControls(res, formatter);
+  }
+
+  private void setupScratchControls(Resources res, NumberFormat formatter) {
+    scratchLabels = new TextView[Roll.NUM_FACES];
+    scratchCounts = new ProgressBar[Roll.NUM_FACES];
+    for (int i = 1; i <= Roll.NUM_FACES; i++) {
+      String labelIdString = String.format(SCRATCH_LABEL_ID_FORMAT, i);
+      int labelId = res.getIdentifier(labelIdString, "id", getPackageName());
+      scratchLabels[i - 1] = findViewById(labelId);
+      scratchLabels[i - 1].setText(formatter.format(i));
+      String countIdString = String.format(SCRATCH_COUNT_ID_FORMAT, i);
+      int countId = res.getIdentifier(countIdString, "id", getPackageName());
+      scratchCounts[i - 1] = findViewById(countId);
+      scratchCounts[i - 1].setProgress(1 + rng.nextInt(7));
+    }
+  }
+
+  private void setupPlayControls() {
+    roller = findViewById(R.id.roller);
+    // TODO Find & wire up dice ImageView objects
+    roller.setOnClickListener(new RollerListener());
+  }
+
+  private void setupPairControls(Resources res, NumberFormat formatter) {
     pairLabels = new TextView[maxPairValue - minPairValue + 1];
     pairCounts = new ProgressBar[maxPairValue - minPairValue + 1];
-    Resources res = getResources();
-    rng = new Random();
-    NumberFormat formatter = NumberFormat.getInstance();
     for (int i = minPairValue; i <= maxPairValue; i++ ){
       String labelIdString = String.format(PAIR_LABEL_ID_FORMAT, i);
-      int labelId = res.getIdentifier(labelIdString, "id", getOpPackageName());
+      int labelId = res.getIdentifier(labelIdString, "id", getPackageName());
       pairLabels[i - minPairValue] = findViewById(labelId);
       pairLabels[i - minPairValue].setText(formatter.format(i));
       String countIdString = String.format(PAIR_COUNT_ID_FORMAT, i);
       int countId = res.getIdentifier(countIdString, "id", getPackageName());
       pairCounts[i - minPairValue] = findViewById(countId);
       pairCounts[i - minPairValue].setProgress(1 + rng.nextInt(10));
-    }
-    roller = findViewById(R.id.roller);
-    rollDisplay = findViewById(R.id.roll_display);
-    roller.setOnClickListener(new RollerListener());
-
-    //Code for scratch
-    scratchLabels = new TextView[Roll.NUM_FACES];
-    scratchCounts = new ProgressBar[Roll.NUM_FACES];
-    for (int i = 0; i <= Roll.NUM_FACES; i++) {
-      String labelIdString = String.format(SCRATCH_LABEL_ID_FORMAT, i);
-      int labelId = res.getIdentifier(labelIdString, "id", getOpPackageName());
-      scratchLabels[i - minScratchValue] = findViewById(labelId);
-      scratchLabels[i - minScratchValue].setText(formatter.format(i));
-      String countIdString = String.format(SCRATCH_COUNT_ID_FORMAT, i);
-      int countId = res.getIdentifier(countIdString, "id", getPackageName());
-      scratchCounts[1 - minScratchValue] = findViewById(countId);
-      scratchCounts[1 - minScratchValue].setProgress(1 + rng.nextInt(7));
     }
   }
 
@@ -77,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onClick(View v) {
       Roll roll = new Roll(rng);
-      rollDisplay.setText(Arrays.toString(roll.getDice()));
+      //TODO Display dice images.
     }
   }
 
